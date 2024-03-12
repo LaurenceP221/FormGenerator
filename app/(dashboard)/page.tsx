ef738138b6b432d8961620954a156236f0122662
LaurenceP221 <20-03039@g.batstate-u.edu.ph>
2024-03-12 16:20:04 +0800

    finished dashboard frontend

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,5 @@
-import { GetFormStats } from "@/actions/form";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { GetFormStats, GetForms } from "@/actions/form";
+import { Card, CardHeader, CardTitle, CardContent, CardDescription, CardFooter } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { ReactNode, Suspense } from "react";
 import { LuView } from "react-icons/lu";
@@ -7,6 +7,15 @@ import { FaWpforms } from "react-icons/fa";
 import { HiCursorClick } from "react-icons/hi";
 import { TbArrowBounce } from "react-icons/tb";
 import { Separator } from "@/components/ui/separator";
+import CreateFormBtn from "@/components/CreateFormBtn";
+import { Form } from "@prisma/client";
+import { Badge } from "@/components/ui/badge";
+import { formatDistance } from "date-fns";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {BiRightArrowAlt} from "react-icons/bi";
+import {FaEdit} from "react-icons/fa";
+
 
 export default function Home() {
   return (
@@ -15,8 +24,19 @@ export default function Home() {
         <CardStatsWrapper />
       </Suspense>
       <Separator className="my-6"/>
-      <h2 className="text-4xl font-bold col-span-2">Your Forms</h2>
+      <h2 className="text-4xl font-bold col-span-2">Your forms</h2>
       <Separator className="my-6"/>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <CreateFormBtn />
+        <Suspense 
+          fallback={[1,2,3,4].map((el) => (
+            <FormCardSkeleton key={el} />
+           ))}
+        >
+          <FormCards />
+        </Suspense>
+
+      </div>
     </div>
   );
 }
@@ -104,3 +124,66 @@ function StatsCard({
         </Card>
       );
     }
+
+function FormCardSkeleton(){
+  return <Skeleton className="border-2 border-primary-/20 h-[190px] w-full" />
+}
+
+async function FormCards(){
+  const forms = await GetForms();
+  return (
+    <>
+      {forms.map(form => (
+        <FormCard key={form.id} form={form} />
+      ))}
+    </>
+  );
+}
+
+function FormCard({form}:{form:Form}){
+  return(
+    <Card className="border-2 border-primary/20 h-[190px] w-full">
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2 justify-between">
+          <span className="truncate font-bold">{form.name}</span>
+          {form.published && <Badge>Published</Badge>}
+          {!form.published && <Badge variant={"destructive"}>Draft</Badge>}
+        </CardTitle>
+        <CardDescription className="flex items-center justify-between text-muted-foreground text-sm">
+          {formatDistance(form.createdAt, new Date(), {
+            addSuffix: true,
+          })}
+          {
+            form.published && (
+              <span className="flex items-center gap-2" >
+                <LuView className="text-muted-foreground"/>
+                <span>{form.visits.toLocaleString()}</span>
+                <FaWpforms className="text-muted-foreground"/>
+                <span>{form.submissions.toLocaleString()}</span>
+              </span>
+            )
+          }
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="h-[20px] truncate text-sm text-muted-foreground">
+          {form.description || "No description"}
+      </CardContent>
+      <CardFooter>
+          {form.published && (
+            <Button asChild className="w-full mt-2 text-md gap-4">
+              <Link href={`/forms/${form.id}`}>
+                View Submissions <BiRightArrowAlt />
+              </Link>
+            </Button>
+          )}
+          {!form.published && (
+            <Button asChild variant={"secondary"} className="w-full mt-2 text-md gap-4">
+              <Link href={`/builder/${form.id}`}>
+                Edit Form <FaEdit />
+              </Link>
+            </Button>
+          )}
+      </CardFooter>
+    </Card>
+  )
+}
