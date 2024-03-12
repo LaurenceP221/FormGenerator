1065909906498983fe11f37f7d5d0255b6ced101
LaurenceP221 <20-03039@g.batstate-u.edu.ph>
2024-03-12 10:26:44 +0800

    sql ok

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div className="container pt-4">Hello</div>
   );
+}
+
+function CardStatsWrapper() {
+  
 }
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,10 +2,12 @@ import Link from 'next/link'
 import React from 'react'
 
 function Logo() {
-  return <Link href={"/"} className='font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 
+  return (
+  <Link href={"/"} className='font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 
   bg-clip-text text-transparent hover:cursor-pointer'>
     PageForm
   </Link>
+  );
 }
 
 export default Logo
