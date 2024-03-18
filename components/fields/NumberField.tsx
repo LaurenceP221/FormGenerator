9e264fe1975056d0170ffa82b8fa421aafbe9c70
LaurenceP221 <20-03039@g.batstate-u.edu.ph>
2024-03-18 15:10:31 +0800

    hi

--- components/fields/NumberField.tsx
+++ components/fields/NumberField.tsx
@@ -271,7 +271,8 @@ function PropertiesComponent({
           control={form.control}
           name="required"
           render={({ field }) => (
-            <FormItem className="flex items-center justify-between rounded-lg border p-3 shadow-sm">
+            <FormItem className="flex items-center justify-between rounded-lg 
+            border p-3 shadow-sm">
               <div className="space-y-0.5">
                 <FormLabel>Required</FormLabel>
                 <FormDescription>
